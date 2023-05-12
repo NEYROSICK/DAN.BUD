c6281027a80329479643b3ad1623c1e7a45a0445
NEYROSICK <dhatak24@gmail.com>
2023-05-12 16:15:21 +0300

    Header & Hero update
    1. Додав мобільне меню у хедері
    2. Додав необхідні іконки
    3. Додав js файл для бургер меню

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
-import * as flsFunctions from "./modules/functions.js"; 
+import * as flsFunctions from "./modules/functions.js";
 import "./modules/jsGalery.js";
+import "./modules/mobile-menu.js";
 flsFunctions.isWebp();
